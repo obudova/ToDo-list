edfc98215e5421b62ad23bd5205c3f448db60b9a
elinsia <elinsia7@gmail.com>
2017-08-02 13:31:32 +0300

    app color changed

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 import ToDoBuilder from './toDoBuilder';
 // import serviceWorker from './sw';
 new ToDoBuilder(document.querySelector('.board'));
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//         navigator.serviceWorker.register('sw.js').then(function(registration) {
+//             // Registration was successful
+//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//         }, function(err) {
+//             // registration failed :(
+//             console.log('ServiceWorker registration failed: ', err);
+//         });
+//     });
+// }
